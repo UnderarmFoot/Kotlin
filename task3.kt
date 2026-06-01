8bfc33b40c34dca1a969959e53e2efbdb253ab57
UnderarmFoot <nikitadeduhin018@gmail.com>
2026-06-01 15:35:51 +0300

    Update task3.kt

--- task3.kt
+++ task3.kt
@@ -1,1 +1,28 @@
+// Ищет первый элемент типа Int в списке Any
+fun List<Any>.findInt(): Int? {
+    for (item in this) {
+        if (item is Int) {
+            return item
+        }
+    }
 
+    return null
+}
+
+// Проверка работы extension-функции на списке с разными типами данных
+fun main() {
+    val list: List<Any> = listOf(
+        "Hello",
+        3.14,
+        true,
+        "Kotlin",
+        42,
+        User("Nikita")
+    )
+
+    println(list.findInt())
+}
+
+data class User(
+    val name: String
+)
